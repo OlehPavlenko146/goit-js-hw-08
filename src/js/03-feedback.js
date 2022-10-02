e4ae40fc54d8f2d50e2fdd65833a30200347382c
Oleh Pavlenko <a310thunderbolt@gmail.com>
2022-10-02 11:04:00 +0300

    adds hw8

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,36 @@
+import throttle from 'lodash.throttle';
 
+const STORAGE_KEY = 'feedback-form-state';
+
+const refs = {
+  form: document.querySelector('.feedback-form'),
+  email: document.querySelector('.feedback-form input'),
+  textarea: document.querySelector('.feedback-form textarea'),
+};
+
+populateFormFields();
+
+const formData = {};
+
+refs.form.addEventListener('submit', onFormSubmit);
+refs.form.addEventListener('input', throttle(onFormInput, 1000));
+
+function populateFormFields(e) {
+  const savedData = localStorage.getItem(STORAGE_KEY);
+  if (savedData) {
+    refs.email.value = JSON.parse(savedData).email;
+    refs.textarea.value = JSON.parse(savedData).message;
+  }
+}
+
+function onFormSubmit(e) {
+  e.preventDefault();
+  console.log(JSON.parse(localStorage.getItem(STORAGE_KEY)));
+  e.currentTarget.reset();
+  localStorage.removeItem(STORAGE_KEY);
+}
+
+function onFormInput(e) {
+  formData[e.target.name] = e.target.value;
+  localStorage.setItem(STORAGE_KEY, JSON.stringify(formData));
+}
